e203bac8616cf025de3d725840840c0be870dabf
nightmac <chrisseithel@me.com>
2025-11-04 04:44:03 -0600

    Update installUWI.sh

--- installUWI.sh
+++ installUWI.sh
@@ -27,9 +27,7 @@ echo '==========================================================================
 
 # install UUGear Web Interface (UWI)
 if [ $ERR -eq 0 ]; then
-  echo '>>> Install/update UWI'
-#  wget https://www.uugear.com/repo/UWI/LATEST -O uwi.zip || ((ERR++))
-#  unzip uwi.zip -d uwi.latest || ((ERR++))
+  echo '>>> Install UWI'
   cd uwi
   chmod +x messanger.sh
   chmod +x websocketd
@@ -42,31 +40,12 @@ if [ $ERR -eq 0 ]; then
   cd ..
   chown -R $SUDO_USER:$(id -g -n $SUDO_USER) uwi || ((ERR++))
   sleep 2
-  
-#  if [ -d "uwi" ]; then
-#    cur_ver=$(cat uwi/common/js/common.js | grep 'var version =' | sed "s/var version = '//" | sed "s/';//")
-#    latest_ver=$(cat uwi.latest/common/js/common.js | grep 'var version =' | sed "s/var version = '//" | sed "s/';//")
-#    echo ''
-#    echo "Current UWI version=${cur_ver}, latest UWI version=${latest_ver}"
-#    if (( $(echo "$latest_ver $cur_ver" | awk '{print ($1 > $2)}') )); then
-#      echo "UUGear Web Interface (UWI) was installed but there is a newer version now."
-#      postfix=$(tr -dc A-Z0-9 </dev/urandom | head -c 3)
-#      echo "The current version will be moved to 'uwi_${postfix}'. [Please delete it after review]"
-#      echo "The latest version will be installed to 'uwi' directory."
-#      mv uwi "uwi_${postfix}"
-#      mv uwi.latest uwi
-#    else
-#      echo "UUGear Web Interface (UWI) is installed and there is no newer version now."
-#      rm -R uwi.latest
-#    fi
-  else
-    mv uwi uwi
-#  fi
+  mv uwi uwi
 fi
 
 echo
 if [ $ERR -eq 0 ]; then
-  echo '>>> All done. Please reboot your Pi to have UWI server running :-)'
+  echo '>>> All done. Please reboot your OPi to have UWI server running :-)'
   echo 'With default settings, the web interface URL is http://max.local:8000/'
   echo "If you have problem accessing UWI from another device, please run the 'diagnose.sh' script in 'uwi' directory."
 else
